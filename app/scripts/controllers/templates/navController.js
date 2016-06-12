a974a8fe88395caf479a8ac88db563eb32d2848d
admin <admin@samuris.com>
2016-06-12 20:58:35 +0200

    KF-5 display dotted css around div, don't used ng-style, only style from standar css

--- app/scripts/controllers/templates/navController.js
+++ app/scripts/controllers/templates/navController.js
@@ -12,24 +12,29 @@ appModule.controller('navUrlController', ['$scope', '$http', 'SelectedId', 'glob
   });
 }]);
 
-
 appModule.controller('empty', ['$scope', '$http', '$resource', '$location', 'SelectedId', 'globalVars','$rootScope',
   function ($scope, $http, $resource, $location, SelectedId, globalVars,$rootScope) {
     var lastParam = SelectedId.dataObj;
     $http.get(globalVars.keemonoUrl + 'page/' + lastParam).success(function (data) {
       $scope.empty = data;
+      $scope.validData = $scope.empty.layout.schema;
       if(typeof $rootScope.mode == 'undefined' || $rootScope.mode =='edit'){
+
+        $scope.validData = $scope.validData.replace(/visualization/g, 'border-style: dashed');
         $scope.visualization = {
           "border-style": "dashed"
         }
       }else{
+        $scope.validData = $scope.validData.replace(/visualization/g, 'border-style: none');
         $scope.visualization = {
           "border-style": "none"
         }
       }
 
+     // console.table($scope.visualization);
 
 
+      console.log($scope.validData);
     }).error(function (data) {
 
     });
